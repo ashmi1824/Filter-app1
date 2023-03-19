a23fa191546b67a847c37bdd1b41a0d908553c7a
ashmi1824 <81317817+ashmi1824@users.noreply.github.com>
2023-03-19 20:49:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+   
+}
+
+function draw(){
+   
+}
+function modelLoaded(){
+    console.log('PoseNet is Initialized');
+}
+
+
+function preload(){
+}
+
+
